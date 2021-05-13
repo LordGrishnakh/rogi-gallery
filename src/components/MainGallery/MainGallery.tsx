8d214cb005c6ced17ee0a461fef252358de61c36
IgorEXE <werewolfofmoscow@yandex.ru>
2021-05-13 21:18:24 +0300

    fetching logic

--- src/components/MainGallery/MainGallery.tsx
+++ src/components/MainGallery/MainGallery.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import classes from "./MainGallery.module.scss";
 
 import CategoryController from "./CategoryController/CategoryController";
@@ -11,6 +11,37 @@ export type categories =
 
 const MainGallery: React.FC = () => {
   const [category, setCategory] = useState<categories>("Категория 1");
+
+  useEffect(() => {
+    let start;
+    let limit = 6;
+    switch (category.split(" ")[1]) {
+      case "1":
+        start = 0;
+        break;
+      case "2":
+        start = 6;
+        break;
+      case "3":
+        start = 12;
+        break;
+      case "4":
+        start = 18;
+        break;
+      default:
+        break;
+    }
+
+    fetch(
+      `https://jsonplaceholder.typicode.com/photos?_start=${start}&_limit=${limit}`
+    )
+      .then((resp) => resp.json())
+      .then((data) => console.log(data));
+
+    // return () => {
+    //   cleanup
+    // }
+  }, [category]);
 
   const changeCategory = (category: categories) => {
     setCategory(category);
