74fae39a75b59a0835f0a0218e1f64e96c165110
IgorEXE <werewolfofmoscow@yandex.ru>
2021-05-13 17:40:22 +0300

    routing and header added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from "react";
-import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import "./App.scss";
+
+import Header from "./components/Header/Header";
 import About from "./components/About/About";
 import MainGallery from "./components/MainGallery/MainGallery";
 import PhotoDescription from "./components/PhotoDescription/PhotoDescription";
@@ -8,22 +11,14 @@ import PhotoDescription from "./components/PhotoDescription/PhotoDescription";
 function App() {
   return (
     <BrowserRouter>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/description">Description</Link>
-        </li>
-      </ul>
-      <Switch>
-        <Route path="/" exact component={MainGallery} />
-        <Route path="/" component={About} />
-        <Route path="/" component={PhotoDescription} />
-      </Switch>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/" exact component={MainGallery} />
+          <Route path="/about" exact component={About} />
+          <Route path="/description" exact component={PhotoDescription} />
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 }
